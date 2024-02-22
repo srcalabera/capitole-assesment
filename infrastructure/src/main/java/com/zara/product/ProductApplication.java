67b53a901c4e38221611ccec5a16df3d3a2cb4ea
diego.aguirre <diego.aguirre@rappi.com>
2024-02-22 14:34:26 -0300

    Split in modules - infrastructure - application - domain

--- infrastructure/src/main/java/com/zara/product/ProductApplication.java
+++ infrastructure/src/main/java/com/zara/product/ProductApplication.java
@@ -1,13 +1,13 @@
-package com.zara.ecommerce;
+package com.zara.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
--- infrastructure/src/main/resources/db/migration/V101__create_price.sql
+++ infrastructure/src/main/resources/db/migration/V101__create_price.sql
@@ -7,6 +7,7 @@ create table  "prices" (
     "end_date" timestamp not null,
     "price_list" bigint not null,
     "product_id" bigint not null,
+    "priority" int not null,
     "price" NUMERIC(20, 2) not null,
     "curr" varchar(3) not null,
     FOREIGN KEY (brand_id) REFERENCES brand(id)
