9c3f1f4a4a3580cba9fce0fafc1de80de4267604
diego.aguirre <diego.aguirre@rappi.com>
2024-02-23 00:20:28 -0300

    Added integration test in base of requirement - Test 1: petición a las 10:00 del día 14 del producto 35455 para la brand 1 (ZARA) - Test 2: petición a las 16:00 del día 14 del producto 35455 para la brand 1 (ZARA) - Test 3: petición a las 21:00 del día 14 del producto 35455 para la brand 1 (ZARA) - Test 4: petición a las 10:00 del día 15 del producto 35455 para la brand 1 (ZARA) - Test 5: petición a las 21:00 del día 16 del producto 35455 para la brand 1 (ZARA)

---
+++ infrastructure/src/test/java/com/zara/product/inbound/controller/PriceFindControllerTest.java
@@ -0,0 +1,148 @@
+package com.zara.product.inbound.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zara.product.model.Price;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PriceFindControllerTest {
+
+    private static final String PRODUCT_PATH = "/product";
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("petición a las 10:00 del día 14 del producto 35455 para la brand 1 (ZARA)")
+    public void getProductInformation() throws Exception {
+        var productId = java.math.BigInteger.valueOf(35455);
+        var brandId = java.math.BigInteger.valueOf(1);
+        var date = LocalDateTime.of(2020, 6, 14, 10, 0, 0);
+
+        MvcResult result = this.mockMvc.perform(get(PRODUCT_PATH + "/" + productId)
+                        .param("brandId", String.valueOf(brandId))
+                        .param("date", String.valueOf(date)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        var price = objectMapper.readValue(responseContent, Price.class);
+
+        assertEquals(BigInteger.valueOf(1), price.priceList());
+        assertEquals(productId, price.productId());
+        assertEquals(brandId, price.brandId());
+        assertEquals(35.50, price.price());
+
+    }
+
+    @Test
+    @DisplayName("petición a las 16:00 del día 14 del producto 35455 para la brand 1 (ZARA)")
+    public void getProductInformation2() throws Exception {
+        var productId = java.math.BigInteger.valueOf(35455);
+        var brandId = java.math.BigInteger.valueOf(1);
+        var date = LocalDateTime.of(2020, 6, 14, 16, 0, 0);
+
+        MvcResult result = this.mockMvc.perform(get(PRODUCT_PATH + "/" + productId)
+                        .param("brandId", String.valueOf(brandId))
+                        .param("date", String.valueOf(date)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        var price = objectMapper.readValue(responseContent, Price.class);
+
+        assertEquals(BigInteger.valueOf(2), price.priceList());
+        assertEquals(productId, price.productId());
+        assertEquals(brandId, price.brandId());
+        assertEquals(25.45, price.price());
+
+    }
+
+    @Test
+    @DisplayName("petición a las 21:00 del día 14 del producto 35455 para la brand 1 (ZARA)")
+    public void getProductInformation3() throws Exception {
+        var productId = java.math.BigInteger.valueOf(35455);
+        var brandId = java.math.BigInteger.valueOf(1);
+        var date = LocalDateTime.of(2020, 6, 14, 21, 0, 0);
+
+        MvcResult result = this.mockMvc.perform(get(PRODUCT_PATH + "/" + productId)
+                        .param("brandId", String.valueOf(brandId))
+                        .param("date", String.valueOf(date)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        var price = objectMapper.readValue(responseContent, Price.class);
+
+        assertEquals(BigInteger.valueOf(1), price.priceList());
+        assertEquals(productId, price.productId());
+        assertEquals(brandId, price.brandId());
+        assertEquals(35.50, price.price());
+
+    }
+
+
+    @Test
+    @DisplayName("petición a las 10:00 del día 15 del producto 35455 para la brand 1 (ZARA)")
+    public void getProductInformation4() throws Exception {
+        var productId = java.math.BigInteger.valueOf(35455);
+        var brandId = java.math.BigInteger.valueOf(1);
+        var date = LocalDateTime.of(2020, 6, 15, 10, 0, 0);
+
+        MvcResult result = this.mockMvc.perform(get(PRODUCT_PATH + "/" + productId)
+                        .param("brandId", String.valueOf(brandId))
+                        .param("date", String.valueOf(date)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        var price = objectMapper.readValue(responseContent, Price.class);
+
+        assertEquals(BigInteger.valueOf(3), price.priceList());
+        assertEquals(productId, price.productId());
+        assertEquals(brandId, price.brandId());
+        assertEquals(30.50, price.price());
+
+    }
+
+
+
+    @Test
+    @DisplayName("petición a las 21:00 del día 16 del producto 35455 para la brand 1 (ZARA)")
+    public void getProductInformation5() throws Exception {
+        var productId = java.math.BigInteger.valueOf(35455);
+        var brandId = java.math.BigInteger.valueOf(1);
+        var date = LocalDateTime.of(2020, 6, 16, 21, 0, 0);
+
+        MvcResult result = this.mockMvc.perform(get(PRODUCT_PATH + "/" + productId)
+                        .param("brandId", String.valueOf(brandId))
+                        .param("date", String.valueOf(date)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseContent = result.getResponse().getContentAsString();
+        var price = objectMapper.readValue(responseContent, Price.class);
+
+        assertEquals(BigInteger.valueOf(4), price.priceList());
+        assertEquals(productId, price.productId());
+        assertEquals(brandId, price.brandId());
+        assertEquals(38.95, price.price());
+
+    }
+}
